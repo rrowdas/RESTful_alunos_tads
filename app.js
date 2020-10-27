26e48cb180ad457ee88e0931b1ffb58510ab53d1
rrowdas <rdr.roger@gmail.com>
2020-10-26 23:05:58 -0400

    morgan module pra ver as req com nodemon

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
 const app = express();
+const morgan = require('morgan');
 
 const rotaAlunos = require('./routes/alunos');
 
-
+app.use(morgan('dev'));
 app.use('/alunos', rotaAlunos);
 
 
